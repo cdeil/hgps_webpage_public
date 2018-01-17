1ab0400c1b1968e3fc3256d58756cd6c336ce0d5
Christoph Deil <Deil.Christoph@gmail.com>
2018-01-17 15:30:02 +0100

    update

--- make.py
+++ make.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
+import os
+import hashlib
+from pathlib import Path
+from shutil import copyfile
+import yaml
+import jinja2
 import click
 
+CONFIG = yaml.load(open('config.yaml'))
+VERSION = CONFIG['version']
+HGPS_ANALYSIS_DIR = Path(os.environ['HGPS_ANALYSIS'])
+HGPS_DATA_DIR = Path(os.environ['HGPS_DATA'])
+FILENAME_CAT = f'hgps_catalog_v{VERSION}.fits.gz'
 
 @click.group()
 def cli():
@@ -30,23 +41,44 @@ def clean():
 def build():
     """Build webpage"""
     print('===> Executing task: build')
-    build_html()
     build_data()
     build_images()
+    build_html()
 
 
 def build_html():
     print('---> build_html')
-    # TODO: copy & fill templates in `build`
+    ctx = {}
+    ctx['config'] = CONFIG
+    ctx['catalog'] = make_file_info(FILENAME_CAT)
+
+    env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader('src'),
+        undefined=jinja2.StrictUndefined,
+    )
+    template = env.get_template('index.html')
+    text = template.render(ctx)
+
+    Path('build/index.html').write_text(text)
+
+    copyfile('src/bootstrap.min.css', 'build/bootstrap.min.css')
+    copyfile('src/style.css', 'build/style.css')
 
 
 def build_data():
     print('---> build_data')
-    # TODO: copy data files in `build/data`
+    out_path = Path('build/data')
+    out_path.mkdir(exist_ok=True)
+    copyfile(HGPS_ANALYSIS_DIR / 'data/catalogs/HGPS3/release/HGPS_v0.4.fits', out_path / FILENAME_CAT)
 
+    for m in CONFIG['maps']:
+        m['filename'] = f'hgps_map_{m["quantity"]}_{m["radius"]}deg_v{VERSION}.fits.gz'
+        copyfile(HGPS_DATA_DIR / 'release' / m['in'], out_path / m['filename'])
+    exit()
 
 def build_images():
     print('---> build_images')
+    Path('build/images').mkdir(exist_ok=True)
     # TODO: copy image files in `build/images`
 
 
@@ -77,6 +109,19 @@ def run(cmd):
     from subprocess import call
     print('Executing: {}'.format(cmd))
     call(cmd, shell=True)
+
+
+def make_file_info(filename):
+    info = dict()
+    info['filename'] = filename
+    info['path'] = 'data/' + info['filename']
+    path = Path('build') / info['path']
+    mb = path.stat().st_size / 1024 ** 2
+    info['filesize'] = f'{mb:.1f} MB'
+    md5 = hashlib.md5(path.read_bytes()).hexdigest()
+    info['md5'] = md5
+    info['html'] = '<a href="{path}" download>{filename}</a> ({filesize}, MD5: {md5})</li>'.format_map(info)
+    return info
 
 
 if __name__ == '__main__':
