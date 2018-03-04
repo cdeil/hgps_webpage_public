56ead8c60673140d910fc2129b71fca38715b121
Christoph Deil <Deil.Christoph@gmail.com>
2018-03-04 19:20:57 +0100

    update

--- check.py
+++ check.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
+from pathlib import Path
 from astropy.io import fits
+from astropy.table import Table
 import click
 import utils
 from make import config
@@ -23,7 +25,19 @@ def check_dump_tables_to_text():
     filename = config.out_path / config.filename_cat
     print(f'Reading {filename}')
     hdu_list = fits.open(str(filename))
-    import IPython; IPython.embed()
+
+    path = Path('checks/tables')
+    path.mkdir(exist_ok=True, parents=True)
+
+    for hdu in hdu_list[1:]:
+        # import IPython; IPython.embed()
+        table = Table.read(hdu)
+        data = utils.table_to_list_of_dict(table)
+        path = Path(f'checks/tables/{hdu.name}.json')
+        print(f'Writing {path}')
+        utils.write_json(data, path)
+
+    # import IPython; IPython.embed()
 
 
 if __name__ == '__main__':
