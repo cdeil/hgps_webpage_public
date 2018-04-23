7af522e6eb70b5e3bd110b101d78633049412cd1
Christoph Deil <Deil.Christoph@gmail.com>
2018-04-23 15:21:24 +0200

    Change Hips to galactic frame

--- make_hips_survey_maps.py
+++ make_hips_survey_maps.py
@@ -11,14 +11,24 @@ from make_extra_survey_maps import get_sky_image
 log = logging.getLogger(__name__)
 
 HIPSGEN_OPTIONS = {
-    'creator_did': 'ivo://XXX',
+    'creator_did': 'MPIK/P/HGPS',
     'blank': '0',
+    'hips_frame': 'galactic',
+}
+
+HIPSGEN_OPTIONS_SIGNIFICANCE = {
+    **HIPSGEN_OPTIONS,
+    'label': 'HGPS_Significance',
+}
+
+HIPSGEN_OPTIONS_FLUX = {
+    **HIPSGEN_OPTIONS,
+    'label': 'HGPS_Flux',
 }
 
 HIPS_META_INFO = {
     'obs_regime': 'Gamma-ray',
     'obs_collection': 'HGPS',
-    'obs_title': 'HGPS Integral Flux >1TeV',
     'obs_description': ('H.E.S.S. is an array of ground-based gamma-ray telescopes located '
                         'in Namibia. The H.E.S.S. Galactic Plane Survey (HGPS) is the first '
                         'deep and wide survey of the Milky Way in TeV gamma-rays. The flux '
@@ -35,7 +45,17 @@ HIPS_META_INFO = {
     'em_min': '1.240e-18',
     'bib_reference': '2018A&A...612A...1H',
     'bib_reference_url': 'https://ui.adsabs.harvard.edu/#abs/2018A%26A...612A...1H',
-    'hips_creator': 'XXX (A.Donath and C.Deil)',
+    'hips_creator': 'Christoph Deil, Axel Donath, Pierre Fernique',
+}
+
+HIPS_META_INFO_SIGNIFICANCE = {
+    **HIPS_META_INFO,
+    'obs_title': 'HGPS significance (Gaussian sigma)',
+}
+
+HIPS_META_INFO_FLUX = {
+    **HIPS_META_INFO,
+    'obs_title': 'HGPS integral flux > 1 TeV',
 }
 
 
@@ -52,6 +72,25 @@ class Config:
     def in_fits(self):
         return self.in_path / 'image.fits'
 
+    @property
+    def hips_meta_info(self):
+        if self.quantity == 'significance':
+            return HIPS_META_INFO_SIGNIFICANCE
+        elif self.quantity == 'flux':
+            return HIPS_META_INFO_FLUX
+        else:
+            raise ValueError()
+
+    @property
+    def hipsgen_options(self):
+        if self.quantity == 'significance':
+            return HIPSGEN_OPTIONS_SIGNIFICANCE
+        elif self.quantity == 'flux':
+            return HIPSGEN_OPTIONS_FLUX
+        else:
+            raise ValueError()
+
+
 config = Config()
 
 
@@ -60,7 +99,7 @@ def prepare_inputs():
     image = get_sky_image(config.quantity, normed=False)
     filename = config.in_fits
     log.info(f'Writing {filename}')
-    image.write(filename)
+    image.write(filename, overwrite=True)
 
 
 def get_aladin_jar():
@@ -71,9 +110,9 @@ def get_aladin_jar():
     return os.environ['ALADIN_JAR']
 
 
-def run_hipsgen(hipsgen_options):
+def run_hipsgen(opts):
     aladin_jar = get_aladin_jar()
-    cmd = f'java -Xmx1400m -jar {aladin_jar} -hipsgen {hipsgen_options}'
+    cmd = f'java -Xmx1400m -jar {aladin_jar} -hipsgen {opts}'
     log.info(f'Executing command {cmd}')
     subprocess.call(cmd, shell=True)
 
@@ -88,13 +127,13 @@ def generate_hips():
     elif config.quantity == 'significance':
         options['hips_pixel_cut'] = '"0 100 log"'
 
-    options.update(HIPSGEN_OPTIONS)
+    options.update(config.hipsgen_options)
 
-    hipsgen_options = ''
+    opts = ''
     for key, value in options.items():
-        hipsgen_options += f' {key}={value}'
+        opts += f' {key}={value}'
 
-    run_hipsgen(hipsgen_options)
+    run_hipsgen(opts)
 
 
 def view_hips():
@@ -127,11 +166,11 @@ def update_hips_properties():
                 footer = line + fh.read()
 
             key, value = key.strip(), value.strip()
-            if key.replace('#', '') in HIPS_META_INFO:
+            if key.replace('#', '') in config.hips_meta_info:
                 key = key.replace('#', '')
             properties[key] = value
 
-        properties.update(HIPS_META_INFO)
+        properties.update(config.hips_meta_info)
         fh.seek(0)
         lines = ["{key:20s} = {value}".format(key=key, value=value) for key, value in properties.items()]
         lines.append(footer)
@@ -140,19 +179,26 @@ def update_hips_properties():
 
 
 @click.command()
-@click.option('--quantity', default='significance', type=click.Choice(['significance', 'flux']))
+@click.option('--quantity', default='all', type=click.Choice(['significance', 'flux', 'all']))
 def cli(quantity):
     """Generate HIPS for HGPS."""
     logging.basicConfig(level='INFO')
-    config.quantity = quantity
-    config.in_path.mkdir(parents=True, exist_ok=True)
-    config.out_path.mkdir(parents=True, exist_ok=True)
 
-    prepare_inputs()
+    if quantity == 'all':
+        quantities = ['significance', 'flux']
+    else:
+        quantities = [quantity]
 
-    clean_hips()
-    generate_hips()
-    update_hips_properties()
+    for quantity in quantities:
+        config.quantity = quantity
+        config.in_path.mkdir(parents=True, exist_ok=True)
+        config.out_path.mkdir(parents=True, exist_ok=True)
+
+        # prepare_inputs()
+
+        clean_hips()
+        generate_hips()
+        update_hips_properties()
 
 
 if __name__ == '__main__':
